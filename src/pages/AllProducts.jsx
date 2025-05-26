dd2b0ecb541f1a14d7827daeb7a803ceb0147794
amanverma2065 <amanv6932@gmail.com>
2025-05-27 01:07:09 +0530

    modify product

--- src/pages/AllProducts.jsx
+++ src/pages/AllProducts.jsx
@@ -23,18 +23,21 @@ function AllProducts() {
 
   return (
     <div className='pt-16 pb-10 flex flex-col px-6 bg-purple-50'>
-      <div className="flex flex-col items-center md:items-start mb-8 ">
-        <h2 className="text-2xl md:text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-indigo-600 to-purple-600 uppercase tracking-wide mx-2">
-          All Products
-        </h2>
-        <div className="w-32 h-1.5 bg-gradient-to-r from-indigo-600 to-purple-600 rounded-full mx-4 md:mx-8 mt-4 md:mt-4"></div>
-      </div>
+
 
       {(filteredProducts.length > 0) ? (
-        <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3 md:gap-6 lg:grid-cols-5 mt-6'>
-          {filteredProducts.filter((product) => product).map((product, index) => (
-            <ProductCard product={product} key={product._id} />
-          ))}
+        <div>
+          <div className="flex flex-col items-center md:items-start mb-8 ">
+            <h2 className="text-2xl md:text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-indigo-600 to-purple-600 uppercase tracking-wide mx-2">
+              All Products
+            </h2>
+            <div className="w-32 h-1.5 bg-gradient-to-r from-indigo-600 to-purple-600 rounded-full mx-4 md:mx-8 mt-4 md:mt-4"></div>
+          </div>
+          <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3 md:gap-6 lg:grid-cols-5 mt-6'>
+            {filteredProducts.filter((product) => product).map((product, index) => (
+              <ProductCard product={product} key={product._id} />
+            ))}
+          </div>
         </div>
       ) : (<div className="flex flex-col items-center justify-center min-h-[60vh] px-4">
         <h1 className="text-3xl md:text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-indigo-600 to-purple-600 mb-4">
